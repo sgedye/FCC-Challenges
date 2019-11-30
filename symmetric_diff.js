312a6649e6624d579d01765101288b674310845b
Shaun Gedye <sgedye88@yahoo.com.au>
2019-11-30 15:06:24 +1100

    Update symmetric_diff.js

--- symmetric_diff.js
+++ symmetric_diff.js
@@ -1,5 +1,27 @@
 // Find the Symmetric Difference in two or more Arguments.
+// Note all attemps listed here work (i.e. give the correct answer)
 
+
+/* --- Attempt 2 --- */
+function sym(args) {
+  let argsArr = Array.prototype.slice.call(arguments);
+  if (argsArr.length < 2) { return null; }
+  while (argsArr.length >= 2) {
+    //Definine sets using the spread operator, similar to arrays, but each element is listed only once.
+    let set1 = [...new Set (argsArr.shift())];
+    let set2 = [...new Set (argsArr.shift())];
+    //returnArr is filed with items that appear in only one of the sets
+    let returnArr = [...set1.filter(el => !set2.includes(el))].concat([...set2.filter(el => !set1.includes(el))]);
+    //add returnArr to the front of the argsArr to be shifted on the next iteration
+    argsArr.unshift(returnArr);
+  }
+  //argsArr[0] will always be the returnArr, if 2+ arguments were provided
+  return argsArr[0];
+}
+sym([1,3,2,1,9,8], [5,2,1,1,1,4,9], [8,8,8,8], [9,5], [1]); //Returns [3,4,9,1]
+
+
+/* --- Attempt 1 --- */
 /* 
 ** For each arg in arguments, sort the arg array, then filter to only show each number once (i.e. [1,1,2,2,2,3,4] => [1,2,3,4])
 ** Then merge the arg arrays from each argument (i.e. mergedArr = arr1 + arr2 + arr3 + ...)
